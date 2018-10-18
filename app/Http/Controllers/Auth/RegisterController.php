a98ffdcdb08b1df1423e7c94732acdc63cd8c265
ti00qq <ti00qq@gmail.com>
2018-10-18 13:10:16 +0000

    user registration

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -26,8 +26,8 @@ class RegisterController extends Controller
      * Where to redirect users after registration.
      *
      * @var string
-     */
-    protected $redirectTo = '/home';
+     */    
+     protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
